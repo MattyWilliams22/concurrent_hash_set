8e9b1378cedc7748d3109d85828c9d4ed7173be4
Matty Williams <mw2122@doc.ic.ac.uk>
2024-10-22 15:28:08 +0100

    Implement sequential hash set functions

--- skeleton/src/hash_set_sequential.h
+++ skeleton/src/hash_set_sequential.h
@@ -2,33 +2,55 @@
 #define HASH_SET_SEQUENTIAL_H
 
 #include <cassert>
+#include <functional>
 
 #include "src/hash_set_base.h"
 
 template <typename T>
 class HashSetSequential : public HashSetBase<T> {
  public:
-  explicit HashSetSequential(size_t /*initial_capacity*/) {}
-
-  bool Add(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
+  explicit HashSetSequential(size_t initial_capacity) {
+    set_size = 0;
+    for (size_t i = 0; i < initial_capacity; i++) {
+      std::vector<T> v;
+      table.push_back(v);
+    }
   }
 
-  bool Remove(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
+  bool Add(T elem) final {
+    bool result = false;
+    size_t bucket = std::hash<T>()(elem) % table.size();
+    result = std::find(table[bucket].begin(), table[bucket].end(), elem) !=
+             table[bucket].end();
+    table[bucket].push_back(elem);
+    set_size = result ? set_size + 1 : set_size;
+    return result;
   }
 
-  [[nodiscard]] bool Contains(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
+  bool Remove(T elem) final {
+    bool result = false;
+    size_t bucket = std::hash<T>()(elem) % table.size();
+    result = std::find(table[bucket].begin(), table[bucket].end(), elem) !=
+             table[bucket].end();
+    table[bucket].erase(
+        std::remove(table[bucket].begin(), table[bucket].end(), elem),
+        table[bucket].end());
+    set_size = result ? set_size - 1 : set_size;
+    return result;
   }
 
-  [[nodiscard]] size_t Size() const final {
-    assert(false && "Not implemented yet");
-    return 0u;
+  [[nodiscard]] bool Contains(T elem) final {
+    size_t bucket = std::hash<T>()(elem) % table.size();
+    bool result = std::find(table[bucket].begin(), table[bucket].end(), elem) !=
+                  table[bucket].end();
+    return result;
   }
+
+  [[nodiscard]] size_t Size() const final { return set_size; }
+
+ private:
+  std::vector<std::vector<T> > table;
+  size_t set_size;
 };
 
 #endif  // HASH_SET_SEQUENTIAL_H
